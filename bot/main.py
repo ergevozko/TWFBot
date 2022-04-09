1ee8a5b03071c468252aa9bbc19a7a5a9bad256e
Erge Vozko <96722959+ergevozko@users.noreply.github.com>
2022-04-09 11:59:40 +0700

    change client to bot

--- bot/main.py
+++ bot/main.py
@@ -3,12 +3,11 @@ import discord.ext
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="teawaffle?")
-client = discord.Client()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="VTubers"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="VTubers"))
     print(f"Logged in as {bot.user.name}({bot.user.id})")
     
 @bot.command()
