c5eb34e74e31a91108720cade2d77a8bbede9f36
Erge Vozko <96722959+ergevozko@users.noreply.github.com>
2022-04-09 11:34:45 +0700

    Update bot activity

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,16 @@
 import os
+import discord.ext
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="teawaffle?")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+    
+activity = discord.Activity(name='VTuber', type=discord.ActivityType.watching)
+client = discord.Client(activity=activity)
 
 @bot.command()
 async def ping(ctx):
