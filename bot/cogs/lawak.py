f6316d8034e374593d708fba989e54970fc0977f
ergevozko <bakaerge@outlook.com>
2022-04-10 15:41:25 +0700

    Rubah semua! Karena sekarang mendukung Cogs. No error ya plis!

---
+++ bot/cogs/lawak.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+
+class Lawak(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @command.Cog.listener()
+    async def on_ready(self):
+        print('TWFBot ready for ngeLawak!')
+
+    # >teawaffle_ping
+    @command.command()
+    async def ping(self, ctx):
+        await ctx.send("pong?")
+
+    # >teawaffle_hi
+    @command.command()
+    async def hi(self, ctx):
+        await ctx.send("Hi juga. Jangan ganggu ya plis! Lagi nonton VTuber idola nih!")
+
+    # >teawaffle_halo
+    @command.command()
+    async def halo(self, ctx):
+        await ctx.send("Halo juga. Jangan ganggu ya plis! Lagi nonton VTuber idola nih!")
+
+    # >teawaffle_simp
+    @command.command()
+    async def simp(self, ctx):
+        await ctx.send("Masalah? Emang bot ga bole nge-simp?")
+
+    # >teawaffle_tes
+    @command.command()
+    async def tes(self, ctx):
+        await ctx.send("Ti... ga jadi 😝")
+
+    # >teawaffle_iloveyou
+    @command.command()
+    async def iloveyou(self, ctx):
+        await ctx.send("I... I hate you! BAKA!")
+
+def setup(client):
+    client.add_cog(Lawak(client))
--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,28 @@
 import discord
 import os
-import command.custom 
+from discord.ext import commands
 
-
-client = discord.Client()
 TOKEN = os.getenv("DISCORD_TOKEN")
+client = commands.Bot(command_prefix=">teawaffle_")
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="VTUBERS"))
     print(f"Logged in as {client.user.name}({client.user.id})")
+
+@client.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if.filename.endswith('.py'):
+        client.load_extension(f'cogs{filename[:-3]}')
 
 if __name__ == "__main__":
     client.run(TOKEN)
